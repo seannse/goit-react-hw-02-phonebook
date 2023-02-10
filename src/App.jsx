640bbe05d558db2948c1d9bc6722065d2970d2e7
seannse <seannse@gmail.com>
2023-02-10 22:06:54 +0200

    add hw

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { ContactForm, ContactList, Filter } from './components';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = contactObj => {
+    if (this.state.contacts.some(({ name }) => name === contactObj.name)) {
+      Notify.failure(`${contactObj.name} is already in contacts!`);
+      return;
+    }
+
+    const withIdContact = {
+      id: nanoid(),
+      ...contactObj,
+    };
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, withIdContact],
+    }));
+  };
+
+  handleFilter = ({ target }) => {
+    const searchName = target.value.trim().toLowerCase();
+    this.setState({ filter: searchName });
+  };
+
+  filteredContacts = () => {
+    return this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(this.state.filter)
+    );
+  };
+
+  handleDelete = id => {
+    this.setState({
+      contacts: this.state.contacts.filter(contact => contact.id !== id),
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm addContact={this.addContact} />
+        <Filter handleFilter={this.handleFilter} value={this.state.filter} />
+        <h2>Contacts</h2>
+        {this.state.contacts.length !== 0 ? (
+          <ContactList
+            handleDelete={this.handleDelete}
+            contactArr={this.filteredContacts()}
+          />
+        ) : (
+          <p>Your contacts list is empty</p>
+        )}
+      </div>
+    );
+  }
+}
